661d44ea5af0badea8eb2c22782021fdcc978bd6
Jian Lee <ramosjianlee@gmail.com>
2026-09-11 19:04:40 +0800

    Add new artwork assets to the project
    - Added multiple PNG and JPG files for various artworks, including backgrounds, posters, and character illustrations.
    - The new assets include high-resolution images, with sizes ranging from 2 MB to 24 MB.
    - These assets will enhance the visual quality of the project and provide more content for the user experience.

--- src/components/Artworks.jsx
+++ src/components/Artworks.jsx
@@ -13,8 +13,164 @@ function Projects() {
             illustration, and visual storytelling.
           </p>
         </div>
-        <div className="flex flex-col gap-4 px-8 py-4 md:px-24 lg:px-36 xl:px-72">
-          Artworks Section
+
+        <div className="flex flex-row items-center justify-center w-full gap-4 pb-2 px-8 xl:gap-8 md:px-24 lg:px-36 xl:px-72">
+          <hr className="w-full opacity-30" />
+          <p className="opacity-30">✦</p>
+          <hr className="w-full opacity-30" />
+        </div>
+
+        <div className="w-full columns-2 md:columns-3 sm:columns-2 lg:columns-3 xl:columns-4 px-8 py-2 xl:gap-8 md:px-24 md:py-4 lg:px-36 lg:py-8 xl:px-72 xl:py-12">
+          <img
+            className="py-2"
+            src="src/assets/Artworks/480246677_914541654176119_2352783793277039663_n.jpg"
+            alt="artwork-1"
+          />
+          <img
+            className="py-2"
+            src="src/assets/Artworks/SKPTR03-2.jpg"
+            alt="artwork-2"
+          />
+          <img
+            className="py-2"
+            src="src/assets/Artworks/SKPTR03-13.jpg"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/TRIO2_3.png"
+            alt="artwork-3"
+          />
+          <img
+            className="py-2"
+            src="src/assets/Artworks/FINAL_iyakin_illustration_1.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/BG3_ZEN_2.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/3RD_POSTER_FINAL_2.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/TRIO1_2.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/2ND_POSTER_FINAL_2.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/MAIN_POSTER_BG_FINAL.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/FINAL_iyakin-foreshortening-shot.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/entity_foreshortening_shot_FINAL.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/FINAL-entity-illustrationn-1.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/FINAL_entity-illustration-2.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/FINAL_iyakin_illustration_2.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/BG1_MAYUMI_2.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/TRIO3_1.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/BG_SLYKIDS_FINAL.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/+FINAL_NICO_PORTRAIT.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/entity-formal-shot_FINAL.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/+FINAL_LOGBEE_PORTRAIT.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/BG_BATCH1_1.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/MOB_POSTER_FINAL.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/BG_2_SLYKIDS_FINAL_1.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/+FINAL_SUNNY_PORTRAIT.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/BG2_BALONG_2.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/BG_BATCH1_2.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/iyakin-medium-shot-color.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/BG_BATCH1_3.png"
+            alt="artwork-3"
+          />
+          <img
+            className=" py-2"
+            src="src/assets/Artworks/IMG_1322.jpg"
+            alt="artwork-3"
+          />
         </div>
       </div>
     </>
