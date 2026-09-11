fe403e067b0b473a2396a423a8fe478689322f57
Jian Lee <ramosjianlee@gmail.com>
2026-09-12 06:18:32 +0800

    feat: update layout and styling in Artworks, Education, and Footer components

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -17,7 +17,7 @@ function Projects() {
         <div className="flex flex-col items-center justify-center w-full gap-4 px-8 py-4 lg:items-start lg:justify-start lg:gap-12 md:gap-12 md:flex-row md:px-24 lg:px-36 xl:px-72">
           <div className="">
             <img
-              className="size-64 lg:size-94 xl:size-106 object-cover rounded-lg shadow-[#31302D] shadow-[4px_4px_0px_0px] lg:shadow-[8px_8px_0px_0px]"
+              className="size-64 lg:size-94 xl:size-106 object-cover rounded-lg shadow-[#31302D] shadow-lg"
               src="src/assets/jian-grad-pic.jpeg"
               alt="jian-grad-pic"
             />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import { BsFillMoonFill } from "react-icons/bs";
 
 function Footer() {
   return (
     <>
       <div className="flex flex-col items-center justify-center w-full py-8">
-        <div className="flex flex-row items-center justify-between w-full font-mono xl:gap-8 md:px-24 lg:px-36  xl:px-72 ">
+        <div className="flex flex-row items-center justify-between w-full font-mono xl:gap-8 px-4 md:px-24 lg:px-36  xl:px-72 ">
           <h3 className="flex md:hidden text-xs text-[#F9EEE8] opacity-60">
             © 2026 Jian
           </h3>
           <h3 className="hidden md:flex text-xs text-[#F9EEE8] opacity-60">
             © 2026 Jian Lee Ramos
           </h3>
-          <h3 className="text-xs text-[#F9EEE8] opacity-60">
+          <h3 className="hidden md:flex text-xs text-[#F9EEE8] opacity-60">
             Bulacan, Philippines
           </h3>
+        </div>
+
+        <div className="absolute right-0 px-8 md:px-4 lg:px-8 xl:px-12 opacity-60 hover:opacity-100 transition-all duration-300 active:opacity-60">
+          <button className="p-2 border border-[#F9EEE8]">
+            <BsFillMoonFill className="text-[#F9EEE8]" />
+          </button>
         </div>
       </div>
     </>
